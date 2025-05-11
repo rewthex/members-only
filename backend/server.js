940cd6ca2f949ad5554a015c78ada056aa2707e1
Aaron <trapp.root@gmail.com>
2025-05-10 17:54:19 -0700

    cors

--- backend/server.js
+++ backend/server.js
@@ -3,42 +3,22 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
 import userRouter from "./routes/userRoutes.js";
-import messageRouter from "./routes/messageRoutes.js";
+import messageRouter from "./routes/messageRoutes.js"
 import passport from "passport";
 import configurePassport from "./config/passport.js";
+
 
 const app = express();
 
 configurePassport(passport);
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://feisty-kindness-production-98a8.up.railway.app"
-];
+app.use(cookieParser())
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
+  origin: process.env.CLIENT_ORIGIN,
+  credentials: true
 }));
 
-app.options("*", cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-}));
-
-app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
